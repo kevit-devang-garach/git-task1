725a7f0c8fe80995d2d32127f6d6a42a32d83acc
Devang Kevit <devang_garach@kevit.io>
2022-02-01 10:11:33 +0530

    f1 feature demo file

---
+++ demo.js
@@ -0,0 +1,5 @@
+var st1 = "university, college, class, student";
+var st = st1.split(",");
+var st2 = [6000,600,50];
+
+console.log("1", st[0]," = ",st2[0]/st2[1],st[1],"=", st2[1]/st2[2],st[2],"=",st2[0],st[3])
